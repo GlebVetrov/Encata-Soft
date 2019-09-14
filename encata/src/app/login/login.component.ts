e02196affa0dd66d13362594251bae1d83485ffe
BigGlebovsky <3696732@mail.ru>
2019-09-14 15:17:37 +0300

    Add Login form to Angular app.

---
+++ encata/src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {UserService} from '../user.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm: FormGroup;
+  error = false;
+  hide = true;
+
+  constructor(private userService: UserService) { }
+
+  submit() {
+    if (this.loginForm.valid) {
+      this.userService.getUser(this.loginForm.value)
+        .subscribe(data => console.log(data.ok),
+          error => {
+            this.error = !error.ok;
+          });
+    }
+  }
+
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      userName: new FormControl('', [Validators.required]),
+      userPassword: new FormControl('', [Validators.required])
+    });
+  }
+}
